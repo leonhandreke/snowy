bb6ff6a996b566d4b68f135c74ff45cdf8e71c82
Leon Handreke <leon.handreke@gmail.com>
2011-04-07 13:25:18 +0200

    Add local storage capability to API client

--- site_media/js/api-client.js
+++ site_media/js/api-client.js
@@ -1,6 +1,6 @@
 // A backbone.js based client to interact with the snowy REST API
 
-var SnowyServer = function(baseURL) {
+/*var SnowyServer = function(baseURL) {
   this.baseURL = baseURL + '/api/1.0';
   // Request the user reference
   this.getUserRef();
@@ -101,10 +101,85 @@ _.extend(SnowyServer.prototype, Backbone.Events, {
   }
 });
 
-  Backbone.sync = function(method, model, success, error) {
+/*
+Backbone.sync = function(method, model, success, error) {
   var server = model.server || model.collection.server;
   // Let the server object handle all methods
   server[method](model, success, error);
+};
+*/
+
+
+// Our Store is represented by a single JS object in *localStorage*. Create it
+// with a meaningful name, like the name you'd give a table.
+var Store = function(name) {
+  this.name = name;
+  var store = localStorage.getItem(this.name);
+  this.data = (store && JSON.parse(store)) || {};
+};
+
+_.extend(Store.prototype, {
+
+  // Save the current state of the **Store** to *localStorage*.
+  save: function() {
+    localStorage.setItem(this.name, JSON.stringify(this.data));
+  },
+
+  // Add a model, giving it a (hopefully)-unique GUID, if it doesn't already
+  // have an id of it's own.
+  create: function(model) {
+    if (!model.id) model.id = model.attributes.id = guid();
+    this.data[model.id] = model;
+    this.save();
+    return model;
+  },
+
+  // Update a model by replacing its copy in `this.data`.
+  update: function(model) {
+    this.data[model.id] = model;
+    this.save();
+    return model;
+  },
+
+  // Retrieve a model from `this.data` by id.
+  find: function(model) {
+    return this.data[model.id];
+  },
+
+  // Return the array of all models currently in storage.
+  findAll: function() {
+    return _.values(this.data);
+  },
+
+  // Delete a model from `this.data`, returning it.
+  destroy: function(model) {
+    delete this.data[model.id];
+    this.save();
+    return model;
+  }
+
+});
+
+// Override `Backbone.sync` to use delegate to the model or collection's
+// *localStorage* property, which should be an instance of `Store`.
+Backbone.sync = function(method, model, success, error) {
+
+  var resp;
+  var store = model.localStorage || model.collection.localStorage;
+
+  switch (method) {
+    case "read":    resp = model.id ? store.find(model) : store.findAll(); break;
+    case "create":  resp = store.create(model);                            break;
+    case "update":  resp = store.update(model);                            break;
+    case "delete":  resp = store.destroy(model);                           break;
+  }
+
+  if (resp && success) {
+    success(resp);
+  } else {
+    if (error)
+    error("Record not found");
+  }
 };
 
 var Note = Backbone.Model.extend({
@@ -158,6 +233,10 @@ var Note = Backbone.Model.extend({
       || attrs["last-change-date"] || attrs["pinned"]) {
         this.set({"last-metadata-change-date": Date.now().toISOString()});
       }
+      // _set was successful
+      return true;
+    } else {
+      return false;
     }
   },
 
@@ -179,6 +258,7 @@ var Note = Backbone.Model.extend({
 
 var NotesCollection = Backbone.Collection.extend({
   model: Note,
+  localStorage: new Store('notes'),
   parse: function(response) {
     // Pass on only the notes object if available
     return response['notes'] || response;
@@ -189,7 +269,7 @@ var NotesCollection = Backbone.Collection.extend({
 });
 
 var Notes = new NotesCollection();
-Notes.server = new SnowyServer(location.origin)
+//Notes.server = new SnowyServer(location.origin)
 
 // Stolen from the backbone.js source
 var getUrl = function(object) {
