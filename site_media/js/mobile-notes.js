fc37e34ed0f5a3d14b672763b07f68cb4155b35d
Leon Handreke <leon.handreke@gmail.com>
2011-04-17 17:21:01 +0200

    Switch to new jQuery mobile based navigation

--- site_media/js/mobile-notes.js
+++ site_media/js/mobile-notes.js
@@ -1,48 +1,20 @@
-var AppController = Backbone.Controller.extend({
-  routes: {
-    '': 'showMainView',
-    'note-list': 'showNoteListView'
-  },
-
-  showMainView: function() {
-    var mainView = new MainView();
-    this.showView(mainView);
-    //var el = $(mainView.render().el);
-    //$('body').append(el);
-  },
-
-  showNoteListView: function() {
-    console.log('note-list');
-    var noteListView = new NoteListView();
-    this.showView(noteListView);
-  },
-
-  showView: function(view) {
-    var el = $(view.render().el);
-    // Append the view to the DOM
-    $('body').append(el);
-    // This weird method of transitioning the view is needed
-    // else jQuery Mobile doesn't do the RightThing(tm)
-    $.mobile.changePage($(el[0]));
-  }
-});
-
-var MainView = Backbone.View.extend({
+var MainPage = Backbone.View.extend({
   events: {
     'click #beginSync': 'beginSync'
   },
   template: function () {
-    return $('#template-main-view');
+    return $('#template-main-page');
   },
   initialize: function() {
+    // init own element
+    this.el = $('<span></span>');
     // Bind render method to always be executed within the context of
     // this object
     _.bindAll(this, 'render');
   },
 
   render: function() {
-    //$(this.el).html(this.template().tmpl());
-    this.el = this.template().tmpl();
+    this.el.html(this.template().tmpl());
     return this;
   },
 
@@ -52,31 +24,32 @@ var MainView = Backbone.View.extend({
   }
 });
 
-var NoteListView = Backbone.View.extend({
+var NoteListPage = Backbone.View.extend({
   events: {
   },
   template: function () {
-    return $('#template-note-list-view');
+    return $('#template-note-list-page');
   },
   initialize: function() {
+    // init own element
+    this.el = $('<span></span>');
     // Bind render method to always be executed within the context of
     // this object
     _.bindAll(this, 'render');
   },
 
   render: function() {
-    this.el = this.template().tmpl();
+    this.el.html(this.template().tmpl());
     return this;
   },
 
 });
 
 $(document).ready(function() {
-  // Disable jQuery Mobile link handling because AppControler does this
-  $.mobile.hashListeningEnabled = false;
-  $.mobile.ajaxEnabled = false;
-  // Set up the AppController
-  var appController = new AppController();
-  Backbone.history.start()
+  var mainPage = new MainPage();
+  $('body').append(mainPage.render().el);
+
+  var noteListPage = new NoteListPage({model: Notes});
+  $('body').append(noteListPage.render().el);
 });
 
